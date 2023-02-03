b18639e1caeb95f55a438ac8a3e56458cea6c649
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-03 17:44:41 -0300

    feat: add image feature added

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
   $("header button").click(() => $("form").slideDown());
-  $("form").on("submit", (event) => event.preventDefault());
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+    const newImageUrl = $("#new-image-url").val();
+    console.log(newImageUrl);
+    const newImageTemplate = `
+    <li>
+      <img src="${newImageUrl}" alt="pintura de um gato">
+      <div class="image-overlay">
+        <a title="ver imagem em tamanho real" href="${newImageUrl}" target="_blank">Ver imagem em tamanhoreal</a>
+      </div>
+    </li>`;
+    $(newImageTemplate).appendTo("ul");
+    $(newImageTemplate).fadeIn();
+    $("#new-image-url").val(" ");
+  });
+
   $("form .cancelar").click(() => $("form").slideUp());
 });
