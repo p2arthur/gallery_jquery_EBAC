789d25512b88a4c4c2cd9301d79d6a3a9519deb3
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-02-04 17:56:42 -0300

    debug: solved the bug on the image fade in

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 $(document).ready(function () {
   $("header button").click(() => $("form").slideDown());
+  $("form .cancelar").click(() => $("form").slideUp());
   $("form").on("submit", (event) => {
     event.preventDefault();
     const newImageUrl = $("#new-image-url").val();
     console.log(newImageUrl);
-    const newImageTemplate = `
-    <li>
-      <img src="${newImageUrl}" alt="pintura de um gato">
-      <div class="image-overlay">
-        <a title="ver imagem em tamanho real" href="${newImageUrl}" target="_blank">Ver imagem em tamanhoreal</a>
-      </div>
-    </li>`;
+    const newImageTemplate = $(`
+    <li style="display: none">
+   
+    <img src="${newImageUrl}" alt="pintura de um gato">
+    <div class="image-overlay">
+      <a title="ver imagem em tamanho real" href="${newImageUrl}" target="_blank">Ver imagem tamanhoreal</a>
+    </div>
+    
+    </li>`);
     $(newImageTemplate).appendTo("ul");
     $(newImageTemplate).fadeIn();
-    $("#new-image-url").val(" ");
+    $("#new-image-url").val("");
   });
-
-  $("form .cancelar").click(() => $("form").slideUp());
 });
+
+function logName(Name, age) {
+  console.log(Name, age);
+  console.log(1234567889000099);
+  console.log(typeof "a" === string);
+}
